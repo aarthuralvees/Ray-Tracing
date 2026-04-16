6c79d34bd473d1ea7a08460598268f2b56a7811e
heitor-brayner <hbp@cin.ufpe.br>
2026-04-15 22:28:15 -0300

    feat: add dedicated test runner under tests/ gated by -DRUN_TESTS

--- main.cpp
+++ main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
+#include <cmath>
 #include "src/Ponto.h"
 #include "src/Vetor.h"
 using namespace std;
 
-int main(){
+#ifdef RUN_TESTS
+#include "tests/test_vetor.h"
+#include "tests/test_ponto.h"
+
+static void run_all_tests() {
+    run_test("vetor_arithmetic",  vetor_arithmetic);
+    run_test("vetor_length",      vetor_length);
+    run_test("vetor_dot_cross",   vetor_dot_cross);
+    run_test("vetor_unit_vector", vetor_unit_vector);
+    run_test("ponto_arithmetic",  ponto_arithmetic);
+    run_test("ray_at",            ray_at);
+    report_tests();
+}
+#endif
+
+int main() {
+#ifdef RUN_TESTS
+    run_all_tests();
+    return tests::_failed > 0 ? 1 : 0;
+#endif
 
     // renderiza uma imagem com um gradiente
     // g++ main.cpp -o a && ./a > image.ppm
-    // xdg-open image.ppm
-    
     int image_width = 256;
     int image_height = 256;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height -j) << ' ' << std::flush;
-        
-        for (int i = 0; i < image_width; i++) {    
-            auto pixel_color = color(double(i)/(image_width -1), double(j)/ (image_height-1), 0);
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
             write_color(std::cout, pixel_color);
-
         }
     }
 
     std::clog << "\rDone.           \n";
-
 }
